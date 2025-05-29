738b17da6089c1d55c741653435b49d371262a4e
yyttgg <52789067+gao-1@users.noreply.github.com>
2025-05-29 14:15:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 accelerate launch --gpu_ids 0 --num_processes 1 --mixed_precision "fp16" \
   tutorial_train_sdxl_ori.py \
-  --pretrained_model_name_or_path="/aigc_data_hdd/checkpoints/stable-diffusion-xl-base-1.0" \
-  --pretrained_ip_adapter_path="/aigc_data_hdd/checkpoints/stable-diffusion-xl-base-1.0/ip-adapter_sdxl.safetensors" \
-  --image_encoder_path="/aigc_data_hdd/checkpoints/stable-diffusion-xl-base-1.0/image_encoder" \
-  --data_root_path='/home/sf/code/IMAGHarmony-2/sdxl-fine-tuning/data/images' \
+  --pretrained_model_name_or_path="your path" \
+  --pretrained_ip_adapter_path="your path" \
+  --image_encoder_path="your path" \
+  --data_root_path='your path' \
   --mixed_precision="fp16" \
   --resolution=512 \
   --train_batch_size=1 \
   --dataloader_num_workers=4 \
   --learning_rate=2.5e-04 \
-  --data_json_file="IMAGHarmony-2/sdxl-fine-tuning/data/train.json" \
+  --data_json_file="your path" \
   --weight_decay=0.01 \
-  --output_dir="/aigc_data_hdd/all_logs/IMAGHarmony_interdim2560_crossheads10_reshapeblocks8_crossvaluedim64" \
+  --output_dir="your path" \
   --save_steps=100 \
   --num_train_epochs 2100 \
   --composed_inter_dim=2560 \
-  --composed_cross_heads=10 \
+  --composed_cross_heads=8 \
   --composed_reshape_blocks=8 \
   --composed_cross_value_dim=64
