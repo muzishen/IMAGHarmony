2532dfd83bde3f20c31d78346d596da3dc0da832
yyttgg <52789067+gao-1@users.noreply.github.com>
2025-05-29 14:02:27 +0800

    Delete assets/新建 文本文档.py

--- assets/新建 文本文档.py
+++
@@ -1,5 +0,0 @@
-from pdf2image import convert_from_path
-
-pages = convert_from_path("C:\\Users\\gao\\Downloads\\assets\\architecture.pdf", dpi=300)
-for i, page in enumerate(pages):
-    page.save(f"page_{i+1}.png", "PNG")
