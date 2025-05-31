bcd83cd6b685b3e551001bdb331e8d9074474d23
yyttgg <52789067+gao-1@users.noreply.github.com>
2025-06-01 01:04:09 +0800

    Delete assets/convert.py

--- assets/convert.py
+++
@@ -1,5 +0,0 @@
-from pdf2image import convert_from_path
-
-pages = convert_from_path("C:\\Users\\gao\\Downloads\\assets\\harmonybench (1).pdf", dpi=300)
-for i, page in enumerate(pages):
-    page.save(f"bench.png", "PNG")
